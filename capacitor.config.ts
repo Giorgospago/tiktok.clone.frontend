97f7fe80801237e0fe4ff7319685c4f5d6cc9cb2
George Pagonoudis <giorgospago23@gmail.com>
2022-01-31 17:37:10 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tiktok.clone.frontend',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
