420b1b0bb37a3ad4900ff2cbdd1c1bdde0826048
George Pagonoudis <giorgospago23@gmail.com>
2022-02-09 21:44:27 +0200

    Update for you

--- src/app/pages/foryou/foryou.module.ts
+++ src/app/pages/foryou/foryou.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { ForyouPageRoutingModule } from './foryou-routing.module';
-
-import { ForyouPage } from './foryou.page';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {ForyouPageRoutingModule} from './foryou-routing.module';
+import {ForyouPage} from './foryou.page';
+import {SwiperModule} from 'swiper/angular';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ForyouPageRoutingModule
-  ],
-  declarations: [ForyouPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ForyouPageRoutingModule,
+        SwiperModule
+    ],
+    declarations: [ForyouPage]
 })
-export class ForyouPageModule {}
+export class ForyouPageModule {
+}
--- src/app/pages/foryou/foryou.page.ts
+++ src/app/pages/foryou/foryou.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import SwiperCore, { Virtual } from 'swiper';
+import {SwiperComponent} from "swiper/angular";
+
+// install Swiper modules
+SwiperCore.use([Virtual]);
 
 @Component({
-  selector: 'app-foryou',
-  templateUrl: './foryou.page.html',
-  styleUrls: ['./foryou.page.scss'],
+    selector: 'app-foryou',
+    templateUrl: './foryou.page.html',
+    styleUrls: ['./foryou.page.scss'],
 })
 export class ForyouPage implements OnInit {
 
-  constructor() { }
+    @ViewChild("TikTokSlides", {static: false})
+    public TikTokSlides: SwiperComponent;
 
-  ngOnInit() {
-  }
+    public page: number = 0;
+    public slides = [];
+
+    constructor(private cd: ChangeDetectorRef) {
+    }
+
+    ngOnInit() {
+        this.initSlides();
+    }
+
+    public initSlides() {
+        this.slides.push(
+            {name: "Slide " + this.getRandomInt()},
+            {name: "Slide " + this.getRandomInt()}
+        );
+    }
+
+    public slideChange(event) {
+        const swipe = event[0];
+        if (swipe.previousIndex < swipe.activeIndex) {
+            // this.slides.push({name: "Slide " + this.getRandomInt()});
+            // if (this.slides.length > 3) {
+            //     this.slides.shift();
+            //     this.TikTokSlides.swiperRef.slidePrev();
+            // }
+            console.log("next");
+        } else {
+            console.log("previous");
+        }
+        this.cd.detectChanges();
+    }
+
+    getRandomInt() {
+        return Math.floor(Math.random() * 1000);
+    }
 
 }
