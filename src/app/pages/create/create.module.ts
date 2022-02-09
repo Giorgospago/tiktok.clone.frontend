d03ef0d5ab8ff70e5e52670e82bad172cc759f89
George Pagonoudis <giorgospago23@gmail.com>
2022-02-09 19:28:31 +0200

    Test video creator

--- src/app/pages/create/create.module.ts
+++ src/app/pages/create/create.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { CreatePageRoutingModule } from './create-routing.module';
-
-import { CreatePage } from './create.page';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {CreatePageRoutingModule} from './create-routing.module';
+import {CreatePage} from './create.page';
+import {TimeagoModule} from "ngx-timeago";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CreatePageRoutingModule
-  ],
-  declarations: [CreatePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        CreatePageRoutingModule,
+        TimeagoModule.forChild()
+    ],
+    declarations: [CreatePage]
 })
-export class CreatePageModule {}
+export class CreatePageModule {
+}
--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -1,18 +1,143 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CreateService} from "../../services/http/create.service";
+import * as RecordRTC from "recordrtc/RecordRTC";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.page.html',
-  styleUrls: ['./create.page.scss'],
+    selector: 'app-create',
+    templateUrl: './create.page.html',
+    styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
 
-  constructor(
-      public createService: CreateService
-  ) { }
+    @ViewChild("videoPreview", {static: true})
+    public videoPreview;
 
-  ngOnInit() {
-  }
+    public startTime: number = null;
 
+    public recorder: RecordRTC;
+    public recordOptions = {
+        // audio, video, canvas, gif
+        type: 'video',
+
+        // audio/webm
+        // audio/webm;codecs=pcm
+        // video/mp4
+        // video/webm;codecs=vp9
+        // video/webm;codecs=vp8
+        // video/webm;codecs=h264
+        // video/x-matroska;codecs=avc1
+        // video/mpeg -- NOT supported by any browser, yet
+        // audio/wav
+        // audio/ogg  -- ONLY Firefox
+        // demo: simple-demos/isTypeSupported.html
+        mimeType: 'video/webm;codecs=h264',
+
+        // MediaStreamRecorder, StereoAudioRecorder, WebAssemblyRecorder
+        // CanvasRecorder, GifRecorder, WhammyRecorder
+        // recorderType: MediaStreamRecorder,
+
+        // disable logs
+        // disableLogs: true,
+
+        // get intervals based blobs
+        // value in milliseconds
+        // timeSlice: 1000,
+
+        // requires timeSlice above
+        // returns blob via callback function
+        // ondataavailable: function (blob) {
+        // },
+
+        // auto stop recording if camera stops
+        // checkForInactiveTracks: false,
+
+        // requires timeSlice above
+        // onTimeStamp: function (timestamp) {
+        // },
+
+        // both for audio and video tracks
+        // bitsPerSecond: 128000,
+
+        // only for audio track
+        // ignored when codecs=pcm
+        // audioBitsPerSecond: 128000,
+
+        // only for video track
+        // videoBitsPerSecond: 128000,
+
+        // used by CanvasRecorder and WhammyRecorder
+        // it is kind of a "frameRate"
+        // frameInterval: 90,
+
+        // if you are recording multiple streams into single file
+        // this helps you see what is being recorded
+        // previewStream: this.previewStream.bind(this),
+
+        // used by CanvasRecorder and WhammyRecorder
+        // you can pass {width:640, height: 480} as well
+        // video: HTMLVideoElement,
+
+        // used by CanvasRecorder and WhammyRecorder
+        canvas: {
+            width: 1080,
+            height: 1920
+        },
+
+        // used by StereoAudioRecorder
+        // the range 22050 to 96000.
+        // sampleRate: 96000,
+
+        // used by StereoAudioRecorder
+        // the range 22050 to 96000.
+        // let us force 16khz recording:
+        // desiredSampRate: 16000,
+
+        // used by StereoAudioRecorder
+        // Legal values are (256, 512, 1024, 2048, 4096, 8192, 16384).
+        // bufferSize: 16384,
+
+        // used by StereoAudioRecorder
+        // 1 or 2
+        // numberOfAudioChannels: 2,
+
+        // used by WebAssemblyRecorder
+        // frameRate: 30,
+
+        // used by WebAssemblyRecorder
+        // bitrate: 128000,
+
+        // used by MultiStreamRecorder - to access HTMLCanvasElement
+        // elementClass: 'multi-streams-mixer'
+    };
+
+    constructor(
+        public createService: CreateService
+    ) {
+    }
+
+    ngOnInit() {
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then((stream: any) => {
+            this.videoPreview.nativeElement.srcObject = stream;
+            this.recorder = RecordRTC(stream, this.recordOptions);
+        });
+    }
+
+    public startRecord() {
+        this.startTime = Date.now();
+        this.recorder.startRecording();
+    }
+
+    public stopRecord() {
+        this.startTime = null;
+        this.recorder.stopRecording(() => {
+            let blob = this.recorder.getBlob();
+            const file = new File([blob], "video");
+
+            this.createService.videoInput.file = file;
+            this.createService.videoInput.path = URL.createObjectURL(file);
+        });
+    }
 }
